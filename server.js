086b37ac7aa20a1d853b9ef3357598537b64537a
Doge in trench coat <124916318+Cassius-K@users.noreply.github.com>
2026-03-15 00:57:19 -0600

    Updated to work with Mongoose V7

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(passport.initialize());
 
 const router = express.Router();
 
-router.post('/signup', function(req, res) {
+router.post('/signup', async function(req, res) {
     if (!req.body.username || !req.body.password) {
         res.json({success: false, msg: 'Please include both username and password to signup.'})
     } else {
@@ -25,15 +25,15 @@ router.post('/signup', function(req, res) {
         user.username = req.body.username;
         user.password = req.body.password;
 
-        user.save(function(err){
-            if (err) {
-                if (err.code == 11000)
-                    return res.json({ success: false, message: 'A user with that username already exists.'});
-                else
-                    return res.json(err);
-            }
+        try {
+            await user.save();
             res.json({success: true, msg: 'Successfully created new user.'})
-        });
+        } catch (err) {
+            if (err.code == 11000)
+                return res.json({ success: false, message: 'A user with that username already exists.'});
+            else
+                return res.json(err);
+        }
     }
 });
 
@@ -42,16 +42,18 @@ router.all('/signup', (req, res) => {
     res.status(405).send({ message: 'HTTP method not supported.' });
 });
 
-router.post('/signin', function (req, res) {
+router.post('/signin', async function (req, res) {
     var userNew = new User();
     userNew.username = req.body.username;
     userNew.password = req.body.password;
 
-    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
-        if (err) {
-            res.send(err);
-        }
+    try {
+        const user = await User.findOne({ username: userNew.username }).select('name username password').exec();
         
+        if (!user) {
+            return res.status(401).send({success: false, msg: 'Authentication failed.'});
+        }
+
         user.comparePassword(userNew.password, function(isMatch) {
             if (isMatch) {
                 var userToken = { id: user.id, username: user.username };
@@ -62,7 +64,9 @@ router.post('/signin', function (req, res) {
                 res.status(401).send({success: false, msg: 'Authentication failed.'});
             }
         })
-    })
+    } catch (err) {
+        res.send(err);
+    }
 });
 
 router.all('/signin', (req, res) => {
@@ -71,54 +75,59 @@ router.all('/signin', (req, res) => {
 });
 
 router.route('/movies/:title')
-    .get(authJwtController.isAuthenticated, function (req, res) {
-        Movie.findOne({title: req.params.title}, function(err, data) {
-            if (err || data.length == 0) {
+    .get(authJwtController.isAuthenticated, async function (req, res) {
+        try {
+            const data = await Movie.findOne({title: req.params.title});
+            if (!data) {
                 res.json({status: 400, message: "Movie ''" + req.params.title + "'' couldn't be found."})
             }
             else {
                 res.json({status: 200, message: "" + req.params.title + " was found!", movie: data});
             }
-        })
+        } catch (err) {
+            res.json({status: 400, message: "Movie ''" + req.params.title + "'' couldn't be found."})
+        }
     })
 
     .post(authJwtController.isAuthenticated, (req, res) => {
         res.json({status: 400, message: "Invalid action."})
     })
 
-    .put(authJwtController.isAuthenticated,function(req, res) {
-        Movie.findOneAndUpdate(
-            {title: req.params.title}, { 
-                title: req.body.title,
-                releaseDate: req.body.releaseDate,
-                genre: req.body.genre,
-                actors: req.body.actors 
-            },
-            { new: true },
-            function(err, doc) {
-                if (err) {
-                    res.json({ message: "Movie could not be updated." });
-                }
-                else if (!doc) {
-                    res.json({ message: "Movie not found." });
-                }
-                else {
-                    res.json({ status: 200, message: "" + req.body.title + " UPDATED"});
-                }
+    .put(authJwtController.isAuthenticated, async function(req, res) {
+        try {
+            const doc = await Movie.findOneAndUpdate(
+                {title: req.params.title}, { 
+                    title: req.body.title,
+                    releaseDate: req.body.releaseDate,
+                    genre: req.body.genre,
+                    actors: req.body.actors 
+                },
+                { new: true }
+            );
+
+            if (!doc) {
+                res.json({ message: "Movie not found." });
             }
-        );
+            else {
+                res.json({ status: 200, message: "" + req.body.title + " UPDATED"});
+            }
+        } catch (err) {
+            res.json({ message: "Movie could not be updated." });
+        }
     })
 
-    .delete(authJwtController.isAuthenticated, function(req, res) {
-        Movie.findOneAndDelete({title: req.params.title}, function(err, data) {
-            if (err || data.length == 0) {
-                res.json(err);
+    .delete(authJwtController.isAuthenticated, async function(req, res) {
+        try {
+            const data = await Movie.findOneAndDelete({title: req.params.title});
+            if (!data) {
                 res.json({message: "There was an issue trying to find your movie"})
             }
             else {
                 res.json({message: "" + req.params.title + " DELETED"});
             }
-        })
+        } catch (err) {
+            res.json(err);
+        }
     })
     
     .all((req, res) => {
@@ -129,44 +138,45 @@ router.route('/movies/:title')
 );
 
 router.route('/movies')
-    .get(authJwtController.isAuthenticated, function (req, res) {
-        Movie.find({}, 'title', function(err, data) {
-            if (err || data.length == 0) {
+    .get(authJwtController.isAuthenticated, async function (req, res) {
+        try {
+            const data = await Movie.find({}, 'title');
+            if (!data || data.length == 0) {
                 res.json({status: 400, message: "No movies found."})
             }
             else {
                 const movieTitles = data.map(movie => movie.title);
                 res.json({status: 200, message: "Movies found!", titles: movieTitles});
             }
-        })
+        } catch (err) {
+            res.json({status: 400, message: "No movies found."});
+        }
     })
     
-    .post(authJwtController.isAuthenticated, function(req, res) {
-        Movie.findOne({title: req.body.title}, function(err) {
-            if (err) {
-                res.status(400);
-            }
-            else if (req.body.actors.length < 3) {
-                res.json({message: "Not enough actors. (You need at least 3)"});
-            }
-            else {
-                var newMovie = new Movie();
-                newMovie.title = req.body.title;
-                newMovie.releaseDate = req.body.releaseDate;
-                newMovie.genre = req.body.genre;
-                newMovie.actors = req.body.actors;
-                
-                newMovie.save(function (err) {
-                    if (err) {
-                    res.json({message: err});
-                    }
-                    else {
-                        res.json({status: 200, success: true, message: "" + req.body.title + " SAVED"});
-                    }
-                });
+    .post(authJwtController.isAuthenticated, async function(req, res) {
+        try {
+            try {
+                await Movie.findOne({title: req.body.title});
+            } catch (err) {
+                return res.status(400);
             }
 
-        });
+            if (!req.body.actors || req.body.actors.length < 3) {
+                return res.json({message: "Not enough actors. (You need at least 3)"});
+            }
+            
+            var newMovie = new Movie();
+            newMovie.title = req.body.title;
+            newMovie.releaseDate = req.body.releaseDate;
+            newMovie.genre = req.body.genre;
+            newMovie.actors = req.body.actors;
+            
+            await newMovie.save();
+            res.json({status: 200, success: true, message: "" + req.body.title + " SAVED"});
+            
+        } catch (err) {
+            res.json({message: err});
+        }
     })
 
     .put(authJwtController.isAuthenticated, (req, res) => {
